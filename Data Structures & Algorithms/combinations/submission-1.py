a58a74ee384f24e2d6e2beec41afbb181f8c87ef
Calvin Chang <me@calvinchang.dev>
2026-09-19 11:35:11 -0400

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        combs = []
+        curCombs = []
+        self.helper(1, curCombs, combs, n, k)
+        return combs
+    
+    def helper(self, i, curCombs, combs, n, k):
+        if len(curCombs) == k:
+            combs.append(curCombs.copy())
+            return
+        for j in range(i, n+1):
+            curCombs.append(j)
+            self.helper(j+1, curCombs, combs, n, k)
+            curCombs.pop()
+        
