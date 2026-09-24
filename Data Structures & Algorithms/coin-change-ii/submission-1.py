2240db9e8bd4faf75ec66be3fece38fdef4d7227
Calvin Chang <me@calvinchang.dev>
2026-09-24 11:17:27 -0400

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        ## Top-down DFS + Memoization
+
+        # memo[(i, remain)] 表示：从 coins[i:] 中凑出 remain 的组合数量
+        memo = {}
+
+        def dfs(i, remain):
+            # 正好凑出目标：当前找到 1 种合法组合
+            if remain == 0:
+                return 1
+
+            # coin 已经用完，或者金额超过目标
+            # 当前路径不合法
+            if i == len(coins) or remain < 0:
+                return 0
+
+            state = (i, remain)
+
+            # 相同状态之前已经计算过
+            if state in memo:
+                return memo[state]
+
+            # 选择 1：使用当前 coin
+            # 因为 coin 可以无限使用，
+            # 所以 i 不变，仍然可以继续使用当前 coin
+            use = dfs(i, remain - coins[i])
+
+            # 选择 2：不再使用当前 coin
+            # 移动到下一种 coin
+            skip = dfs(i + 1, remain)
+
+            # 两类组合互不重复，所以直接相加
+            memo[state] = use + skip
+
+            return memo[state]
+
+        return dfs(0, amount)
