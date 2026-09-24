35dd4a456fdb39394b6d576907c8b3e88dba8074
Calvin Chang <me@calvinchang.dev>
2026-09-24 10:46:52 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        counter = defaultdict(int)
+
+        for n in nums:
+            if n in counter:
+                return True
+            counter[n] = 1
+        return False
+
